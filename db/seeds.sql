18c029083ccd3337b57a734d54fdb3e49aac3a10
MARIO LOPEZ <m-lopez22@live.com>
2020-01-28 19:41:30 -0800

    fixed NPM issues / handelbars

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Gold Standard Burger');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black & Bleu Burger');
+
+INSERT INTO menu (item) VALUES ('Mushroom Swiss Burger');
+INSERT INTO menu (item) VALUES ('Gold Standard Burger');
+INSERT INTO menu (item) VALUES ('Buffalo Burger');
+INSERT INTO menu (item) VALUES ('Rising Sun Burger');
+INSERT INTO menu (item) VALUES ('Billionaire Burger');
+INSERT INTO menu (item) VALUES ('Cheeseburger');
+INSERT INTO menu (item) VALUES ('Al Pastor Burger');
+INSERT INTO menu (item) VALUES ('Chef Special Burger');
+INSERT INTO menu (item) VALUES ('Monster Burger');
+
+INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Cheese Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Garden Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Caesar Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Curly Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Waffle Fries', 0);
